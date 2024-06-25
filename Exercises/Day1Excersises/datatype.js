03954c6125164d0e2b9a8b89771b53cd825232a5
aumpatel3 <aumpatel38@gmail.com>
2024-06-25 01:04:01 -0400

    test

---
+++ Exercises/Day1Excersises/datatype.js
@@ -0,0 +1,4 @@
+console.log(typeof name)
+console.log(typeof man)
+console.log(typeof boat)
+console.log(typeof pet)
---
+++ Exercises/Day1Excersises/variable.js
@@ -0,0 +1,4 @@
+let name = 'Aum'
+let man = true
+let boat = null
+let pet = undefined
